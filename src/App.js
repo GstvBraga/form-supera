31d7725c73ffb877f9639a21bb0d4a568b2de7aa
GstvBraga <gubragarabelo@gmail.com>
2022-04-16 09:49:28 -0300

    Primeiro commit formulário supera em react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import FormSupera from './FormSupera';
+
+function App() {
+  return (
+    <FormSupera />
+  );
+}
+
+export default App;
---
+++ src/FormSupera.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function FormSupera() {
+  return (
+    <div>FormSupera</div>
+  )
+}
