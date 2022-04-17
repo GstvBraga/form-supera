6f3acbc93b3fa27c6366f75c34d6a545402da977
GstvBraga <gubragarabelo@gmail.com>
2022-04-17 17:03:09 -0300

    ultimo commit finalização do projeto

--- README.md
+++ README.md
@@ -1,70 +1,11 @@
-# Getting Started with Create React App
+# Clonagem do repositório
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Clone o repositório do Github para uma pasta local
 
-## Available Scripts
+## Imagens de preview
 
-In the project directory, you can run:
+As imagens de preview estão disponiveis na pasta previews: mobile-form-preview.PNG para a versão mobile e desktop-form-preview.PNG para a versão de desktop
 
-### `npm start`
+## Inicialização do projeto
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Para inicializar o projeto tenha instalado o react e o node js, entre na pasta clonada apartir do terminal utilizando: cd "caminho da pasta form-supera" uma vez dentro da pasta digite o comando npm install para instalar os modulos necessários, em seguida digite npm start, o projeto será iniciado em uma instancia do localhost que aparecerá no terminal, abra seu navegador e copie o link
--- src/FormSupera.js
+++ src/FormSupera.js
@@ -1,37 +1,58 @@
-import React, {useState} from 'react'
+import React, {useState, Component} from 'react'
 import { useForm } from 'react-hook-form'
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content'
+import { ErrorMessage } from '@hookform/error-message';
+
+var formDisableVar = false;
+
+
+const EditButtonShow = (formDisable)=>{
+  //Se o formulário existir no armazenamento locar vai atribuir os valores para a variavel
+  if (!formDisable){
+    return true
+  }else{
+    return false
+  }
+  
+}
 
 export default function FormSupera() {
-  
-  
   //Se o formulário existir no armazenamento locar vai atribuir os valores para a variavel
   if (localStorage.getItem("formSupera")){
     var formSupera = localStorage.getItem("formSupera");
     formSupera = (JSON.parse(formSupera));
-    var formDisableVar = true;
+    formDisableVar = true;
   }else{
-    var formDisableVar = false;
+    formDisableVar = false;
     var formSupera = {};
 }
   //Cria um useState para poder editar o formulário caso esteja desativado
   const [formDisable, setFormDisable]= useState(formDisableVar)
   //Define os valores padrões do formulário como os armazenados no local se existirem
-  const {register, handleSubmit, errors } = useForm({
+  const {register, formState: { errors }, handleSubmit } = useForm({
       defaultValues: {
         nome: formSupera?.nome,
         data: formSupera?.data,
         email: formSupera?.email,
         senha: formSupera?.senha,
         numero: formSupera?.numero,
-        select: formSupera?.select,
+        genero: formSupera?.genero,
         checkbox: formSupera?.checkbox
       }
   });
-  const onSubmit = data =>{
-      console.log(data);
-      localStorage.setItem("formSupera", JSON.stringify(data));
+
+  const onSubmit =  (data)=>{
+    const SubmitFormSwal = withReactContent(Swal)
+    localStorage.setItem("formSupera", {});
+    console.log(data);
+    localStorage.setItem("formSupera", JSON.stringify(data));
+    setFormDisable(true);
+    SubmitFormSwal.fire({
+      title: 'Fomulário salvo',
+      icon: 'success',
+      confirmButtonColor: '#3085d6',
+    })
   }
 
   function editForm(){
@@ -50,71 +71,97 @@ export default function FormSupera() {
       }
     })
   }
-
-  function editButtonShow() 
-  {
-    let editButton;
-    if (formDisable = true){
-    editButton = <EditButton onClick={()=>editForm()}>editar</EditButton>
-  }else{
-    editButton = ''
-  }
-
   
   return (
     <>
+    <div className="content">
     <form onSubmit={handleSubmit(onSubmit)}>
       <h1>Formulário Supera</h1>
       <fieldset disabled={formDisable}>
         <div className="input-field">
           <input
           placeholder=' '
-          type="text" {...register(('nome'), {required:true})}/>
+          type="text" {...register('nome', {
+          required:"Preencha o campo",
+          pattern: {
+            value: /[A-Za-z ]/,
+            message: "Digitos inválidos"
+          }
+          })}/>
           <label htmlFor="nome">Nome</label>
+          <ErrorMessage
+            errors={errors}
+            name="nome"
+            render={({ message }) => <p>{message}</p>}
+          />
         </div>
         <div className="input-field">
-          <input type="date" {...register(('data'),{required:true})}/>
-          <label htmlFor="nome"></label>
+          <input type="date" {...register(('data'),{required:"Preencha o campo"})}/>
+          <label htmlFor="date">Data de nascimento</label>
+          <ErrorMessage
+            errors={errors}
+            name="data"
+            render={({ message }) => <p>{message}</p>}
+          />
         </div>
         <div className="input-field">
           <input 
           placeholder=' ' 
-          type="email" {...register(('email'),{required:true})}/>
+          type="email" {...register(('email'),{required:"Preencha o campo"})}/>
           <label htmlFor="nome">Email</label>
+          <ErrorMessage
+            errors={errors}
+            name="email"
+            render={({ message }) => <p>{message}</p>}
+          />
         </div>
         <div className="input-field">
           <input 
           placeholder=' '
-          type="password" {...register(('senha'),{required:true})}/>
+          type="password" {...register(('senha'),{required:"Preencha o campo"})}/>
           <label htmlFor="nome">Senha</label>
+          <ErrorMessage
+            errors={errors}
+            name="senha"
+            render={({ message }) => <p>{message}</p>}
+          />
         </div>
         <div className="input-field">
           <input 
           placeholder=' '
-          type="number" {...register(('numero'),{required:true})}/>
-          <label htmlFor="nome">Número</label>
+          type="number" {...register(('numero'),{required:"Preencha o campo"})}/>
+          <label htmlFor="nome">Telefone (apenas números)</label>
+          <ErrorMessage
+            errors={errors}
+            name="numero"
+            render={({ message }) => <p>{message}</p>}
+          />
         </div>
         <div className="input-field">
           <select 
           placeholder=' '
-          {...register(('select'),{required:true})}>
+          {...register(('genero'),{required:"Preencha o campo"})}>
             <option value="mulher">Mulher</option>
             <option value="homem">Homem</option>
             <option value="outro">Outro</option>
           </select>
-          <label htmlFor="select">Genero</label>
+          <label htmlFor="genero">Gênero</label>
+          <ErrorMessage
+            errors={errors}
+            name="genero"
+            render={({ message }) => <p>{message}</p>}
+          />
         </div>
         <div className="input-field">
           <input 
-          placeholder=' '
-          type="checkbox" {...register(('checkbox'),{required:true})}/>
-          <label htmlFor="checkbox">aaaa</label>
+          type="checkbox" {...register(('checkbox'), {required:false})}/>
+          <label htmlFor="checkbox">Desejo que a empresa me envie emails com novidades</label>
         </div>
         <button type="submit">Enviar</button>
       </fieldset>
     </form>
-    {editButtonShow()}
+    <button className="edit--btn" onClick={()=>editForm()} disabled={EditButtonShow(formDisable)}>Editar formulário</button>
+    </div>
     </>
   );
-}
-
+  }
