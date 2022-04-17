d11481bc7d5a19f475159c3562ab1cbc97e3a68f
GstvBraga <gubragarabelo@gmail.com>
2022-04-16 23:55:03 -0300

    Criado formulário + layout basico, função de adicionar o formulário ao localstorage + checar se já existe e adicionar os valores caso existentes, botão para editar o formulário caso esteja desabilitado.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import FormSupera from './FormSupera';
+import { useForm } from 'react-hook-form';
 
 function App() {
   return (
+    <>
     <FormSupera />
+    </>
   );
 }
 
--- src/FormSupera.js
+++ src/FormSupera.js
@@ -1,7 +1,90 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useForm } from 'react-hook-form'
 
 export default function FormSupera() {
+  
+  
+  //Se o formulário existir no armazenamento locar vai atribuir os valores para a variavel
+  if (localStorage.getItem("formSupera")){
+    var formSupera = localStorage.getItem("formSupera");
+    formSupera = (JSON.parse(formSupera));
+    var formDisableVar = true;
+  }else{
+    var formDisableVar = false;
+    var formSupera = {};
+}
+  //Cria um useState para poder editar o formulário caso esteja desativado
+  const [formDisable, setFormDisable]= useState(formDisableVar)
+  //Define os valores padrões do formulário como os armazenados no local se existirem
+  const {register, handleSubmit, errors } = useForm({
+      defaultValues: {
+        nome: formSupera?.nome,
+        data: formSupera?.data,
+        email: formSupera?.email,
+        senha: formSupera?.senha,
+        numero: formSupera?.numero,
+        select: formSupera?.select,
+        checkbox: formSupera?.checkbox
+      }
+  });
+  const onSubmit = data =>{
+      console.log(data);
+      localStorage.setItem("formSupera", JSON.stringify(data));
+  }
+
   return (
-    <div>FormSupera</div>
+    <>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h1>Formulário da Supera</h1>
+      <fieldset disabled={formDisable}>
+        <div className="input-field">
+          <input
+          placeholder=' '
+          type="text" {...register(('nome'), {required:true})}/>
+          <label htmlFor="nome">Nome</label>
+        </div>
+        <div className="input-field">
+          <input type="date" {...register(('data'),{required:true})}/>
+          <label htmlFor="nome"></label>
+        </div>
+        <div className="input-field">
+          <input 
+          placeholder=' ' 
+          type="email" {...register(('email'),{required:true})}/>
+          <label htmlFor="nome">Email</label>
+        </div>
+        <div className="input-field">
+          <input 
+          placeholder=' '
+          type="password" {...register(('senha'),{required:true})}/>
+          <label htmlFor="nome">Senha</label>
+        </div>
+        <div className="input-field">
+          <input 
+          placeholder=' '
+          type="number" {...register(('numero'),{required:true})}/>
+          <label htmlFor="nome">Número</label>
+        </div>
+        <div className="input-field">
+          <select 
+          placeholder=' '
+          {...register(('select'),{required:true})}>
+            <option value="mulher">Mulher</option>
+            <option value="homem">Homem</option>
+            <option value="outro">Outro</option>
+          </select>
+          <label htmlFor="select">Genero</label>
+        </div>
+        <div className="input-field">
+          <input 
+          placeholder=' '
+          type="checkbox" {...register(('checkbox'),{required:true})}/>
+          <label htmlFor="checkbox">aaaa</label>
+        </div>
+        <button type="submit">Enviar</button>
+      </fieldset>
+    </form>
+    <button onClick={()=>setFormDisable(false)} type="button">editar</button>
+    </>
   )
 }
